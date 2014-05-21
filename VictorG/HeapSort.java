e0167fa1cd9420ee6a67e3f8f48a22983de361ee
Victor Gaitour <Grimlon1234@gmail.com>
2014-05-20 21:09:11 -0400

    first part of sorting done

--- VictorG/HeapSort.java
+++ VictorG/HeapSort.java
@@ -5,17 +5,24 @@ public class HeapSort {
 	heap=new ALHeap();
     }
     public static Integer[] sort(Integer[] list){
-	for (Integer i: list){
-	    heap.add(i);
-	}
-	
-	Integer[] retArr= new Integer[list.length];
-	int k=0;
-	while (!heap.isEmpty()){
-	    retArr[k]=heap.removeMin();
-	    k++;
-	}
+	for (int i = list.length-1; i>=0;i--){
+	    int k = i;
+	    while (list[k]>list[getParent(k)]){
+		swap(list,k,getParent(k));
+		k=getParent(k);
+	    }
+	}//to make it into a top-down heap
+	//Now that its a heap, we have to 
     }
+    private void swap( Integer[] list, int pos1, int pos2){
+	int a =list[pos1];
+	list[pos1]=list[pos2];
+	list[pos2]=a;
+    }
+    private int getParent(int i){
+	return (i-1)/2;
+    }
+	
     public static void main(String[] args){
 	Integer[] arr= {9,7,8,6,3,4,2,5,1,10};
 	arr=sort(arr);
