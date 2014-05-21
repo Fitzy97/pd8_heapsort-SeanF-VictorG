eafae91e8f2eb6eb6b69b312f14b818d0c6f01bf
Victor Gaitour <victorgaitour@yahoo.com>
2014-05-21 14:01:43 -0400

    heapsort

--- VictorG/HeapSort.java
+++ VictorG/HeapSort.java
@@ -1,32 +1,68 @@
 public class HeapSort {
-    private ALHeap heap;
+    
     
     public HeapSort(){
 	heap=new ALHeap();
     }
-    public static Integer[] sort(Integer[] list){
+    public static int[] sort(int[] list){
 	for (int i = list.length-1; i>=0;i--){
 	    int k = i;
 	    while (list[k]>list[getParent(k)]){
 		swap(list,k,getParent(k));
 		k=getParent(k);
 	    }
-	}//to make it into a top-down heap
-	//Now that its a heap, we have to 
+	}//to make it into a bottom up heap
+	//Now that its a heap, we have to remove the first and move it to the end until the list is sorted
+	partition=list.length;
+	while (partion>0){
+	    swap(list,0,partition);
+	    int k = 0;
+	    while (true){
+		if (list[k]<list[MaxChildPos(heap,list[k])]){
+		    swap(list,k,MaxChildPos(heap,list[k]));
+		    k=MaxChildPos(heap,list[k]);
+		}
+		else
+		    break;
+		partion--;
+	    }
+	}
+	return list;
     }
-    private void swap( Integer[] list, int pos1, int pos2){
+    private static void swap( Integer[] list, int pos1, int pos2){
 	int a =list[pos1];
 	list[pos1]=list[pos2];
 	list[pos2]=a;
     }
-    private int getParent(int i){
+    private static int MaxChildPos( int[] heap ,int pos ) {
+	int retVal;
+	int lc = 2*pos + 1; //index of left child
+	int rc = 2*pos + 2; //index of right child
+	
+	//pos is not in the heap or pos is a leaf position
+	if ( pos < 0 || pos >= heap.size() || lc >= heap.size() )
+	    retVal = -1;
+	//if no right child, then left child is only option for min
+	else if ( rc >= heap.size() )
+	    retVal = lc;
+	//have 2 children, so compare to find least
+	else if ( heap.get(lc).compareTo(heap.get(rc)) > 0 )
+	    retVal = lc;
+	else
+	    retVal = rc;
+	return retVal;
+    }//O(1)
+    
+    private static int getParent(int i){
 	return (i-1)/2;
     }
 	
     public static void main(String[] args){
 	Integer[] arr= {9,7,8,6,3,4,2,5,1,10};
 	arr=sort(arr);
-	System.out.println(arr.toString());
+	for (int i=0;i<arr.length;i++){
+	     System.out.println(arr[i]);
+	}
     }
 	
 	
